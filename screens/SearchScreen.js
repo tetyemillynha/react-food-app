aa6f7307943a0b02e31b3ef4cb3412c94dfce6cf
Stefhany Santos <stefhany.oliveira@beerorcoffee.com>
2023-01-23 20:46:06 -0300

    Calling api using axios

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,27 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../src/api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = ({ navigation }) => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async (searchTerm) =>{
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'san jose'
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage(err);
-    }
-  }
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
